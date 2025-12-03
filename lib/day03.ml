a970ea8851ea25cf8dc3dd06162aaccc1896ff28
Ksawery Radziwiłowicz <ksawery.radziwilowicz@gmail.com>
2025-12-03 08:40:50 +0100

    Day 03 Part 1

--- lib/day03.ml
+++ lib/day03.ml
@@ -1,3 +1,21 @@
 open! Core
 
-let solve _input = print_endline "TBI"
+let rec init a = match a with
+  | []       -> []
+  | [_]      -> []
+  | hd :: tl -> hd :: init tl
+
+let max_joltage bank = match List.mapi bank ~f:(fun i x -> (i, x)) |> init |> List.max_elt ~compare:(fun (_, a) (_, b) -> a - b) with
+  | Some (idx, n) -> 10 * n + (List.drop bank (idx + 1) |> List.max_elt ~compare:(Int.(-)) |> Option.value_exn)
+  | None          -> 0
+
+let part1 banks = List.sum (module Int) banks ~f:max_joltage
+
+let parse_input input = let open List.Monad_infix in
+  String.strip input
+  |> String.split ~on:'\n'
+  >>| (fun s -> String.to_list s
+    >>| (fun x -> Char.to_int x - Char.to_int '0'))
+
+let solve input = let banks = parse_input input in
+  printf "%i\n" (part1 banks)
