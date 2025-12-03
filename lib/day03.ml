0c1aaec0c7dd90ba5b0d7f13f5da455b85832345
Ksawery Radziwiłowicz <ksawery.radziwilowicz@gmail.com>
2025-12-03 08:49:32 +0100

    Day 03 Part 2

--- lib/day03.ml
+++ lib/day03.ml
@@ -1,15 +1,14 @@
 open! Core
 
-let rec init a = match a with
-  | []       -> []
-  | [_]      -> []
-  | hd :: tl -> hd :: init tl
+let rec max_joltage n bank = if n = 0 then
+    0L
+  else
+    match List.mapi bank ~f:(fun i x -> (i, x)) |> (fun a -> List.take a (List.length a - n + 1)) |> List.max_elt ~compare:(fun (_, a) (_, b) -> a - b) with
+    | Some (idx, x) -> let open Int64 in (pow 10L (Int64.of_int n - 1L)) * (Int64.of_int x) + max_joltage (Int.pred n) (List.drop bank (Int.succ idx))
+    | None          -> failwith "Should never happen"
 
-let max_joltage bank = match List.mapi bank ~f:(fun i x -> (i, x)) |> init |> List.max_elt ~compare:(fun (_, a) (_, b) -> a - b) with
-  | Some (idx, n) -> 10 * n + (List.drop bank (idx + 1) |> List.max_elt ~compare:(Int.(-)) |> Option.value_exn)
-  | None          -> 0
-
-let part1 banks = List.sum (module Int) banks ~f:max_joltage
+let part1 banks = List.sum (module Int64) banks ~f:(max_joltage 2)
+let part2 banks = List.sum (module Int64) banks ~f:(max_joltage 12)
 
 let parse_input input = let open List.Monad_infix in
   String.strip input
@@ -18,4 +17,4 @@ let parse_input input = let open List.Monad_infix in
     >>| (fun x -> Char.to_int x - Char.to_int '0'))
 
 let solve input = let banks = parse_input input in
-  printf "%i\n" (part1 banks)
+  printf "%s %s\n" (part1 banks |> Int64.to_string) (part2 banks |> Int64.to_string)
