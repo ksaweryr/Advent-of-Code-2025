145d9800e8643e81b179cf345b4d9b76660409b1
Ksawery Radziwiłowicz <ksawery.radziwilowicz@gmail.com>
2025-12-05 10:38:24 +0100

    Day 05 Part 2

--- lib/day05.ml
+++ lib/day05.ml
@@ -5,9 +5,34 @@ type range = {
   hi : int;
 }
 
+type merge_error = LO | HI
+
 let contains { lo ; hi } n = n >= lo && n <= hi
 
+let num_elements { lo ; hi } = hi - lo + 1
+
+let merge_ranges { lo = alo ; hi = ahi } { lo = blo ; hi = bhi } =
+  if bhi < alo then
+    Error LO
+  else if blo > ahi then
+    Error HI
+  else
+    Ok { lo = min alo blo ; hi = max ahi bhi }
+
+let rec add_range ranges r =
+  match ranges with
+    | [] -> [r]
+    | hd :: tl ->
+      match merge_ranges hd r with
+        | Error LO -> r :: hd :: tl
+        | Error HI -> hd :: add_range tl r
+        | Ok nr    -> add_range tl nr
+
 let part1 database queries = List.count queries ~f:(fun q -> List.exists database ~f:(Fun.flip contains q))
+
+let part2 database =
+  List.fold database ~init:[] ~f:add_range
+  |> List.sum (module Int) ~f:num_elements
 
 let range_of_string line =
   match String.split line ~on:'-' with
@@ -25,4 +50,4 @@ let parse_input input =
 
 let solve input =
   let (database, queries) = parse_input input in
-  printf "%i\n" (part1 database queries)
+  printf "%i %i\n" (part1 database queries) (part2 database)
