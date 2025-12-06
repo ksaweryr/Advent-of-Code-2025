756ed115edee59a40106db269fb9575ef552de67
Ksawery Radziwiłowicz <ksawery.radziwilowicz@gmail.com>
2025-12-06 12:38:55 +0100

    Day 06 Part 2

--- lib/day06.ml
+++ lib/day06.ml
@@ -1,12 +1,12 @@
 open! Core
 
 module Problem = struct
-  type operation = ADD | MUL
+  type operation = ADD | MUL [@@deriving compare, sexp]
 
   type t = {
     numbers : int list;
     operation : operation;
-  }
+  } [@@deriving compare, sexp]
 
   let operation_of_string s = match s with
     | "+" -> ADD
@@ -17,27 +17,53 @@ module Problem = struct
     | ADD -> (0, Int.(+))
     | MUL -> (1, Int.( * ))
 
-  let rec parse a = match a with
+  let rec parse_1 = function
     | []       -> invalid_arg "Cannot parse a problem from an empty list"
     | [op]     -> { numbers = [] ; operation = operation_of_string op }
-    | hd :: tl -> let { numbers ; operation } = parse tl in { numbers = int_of_string hd :: numbers ; operation }
-  
+    | hd :: tl -> let { numbers ; operation } = parse_1 tl in { numbers = int_of_string hd :: numbers ; operation }
+
+  let parse_2 = function
+    | []       -> invalid_arg "Cannot parse a problem from an empty list"
+    | (hd :: _) as cols ->
+      let numbers =
+        let open List.Monad_infix in
+        cols >>| List.drop_last_exn
+        |> List.map ~f:(fun col -> List.filter col ~f:Char.is_digit)
+        >>| (Fun.compose int_of_string String.of_list)
+      and operation = (List.last_exn hd |> Char.to_string |> operation_of_string) in
+      { numbers ; operation }
+
   let evaluate { numbers ; operation } =
     let (init, f) = monoid_of_operation operation in
     List.fold numbers ~init ~f
 end
 
-let part1 problems = List.sum (module Int) problems ~f:Problem.evaluate
+let solve problems = List.sum (module Int) problems ~f:Problem.evaluate
 
-let parse_input input =
+let parse_input_1 input =
   let open List.Monad_infix in
   String.strip input
   |> String.split_lines
   >>| (fun s -> String.split s ~on:' ' |> List.filter ~f:(Fun.compose not String.is_empty))
-  |> List.transpose
-  |> Option.value_exn
-  >>| Problem.parse
+  |> List.transpose_exn
+  >>| Problem.parse_1
+
+let rec group_problems_2 = function
+  | [] -> []
+  | xss ->
+    let cur = List.take_while xss ~f:(fun xs -> List.exists xs ~f:(Char.(<>) ' ')) in
+    cur :: group_problems_2 (List.drop xss (List.length cur + 1))
+
+let parse_input_2 input =
+  let open List.Monad_infix in
+  String.strip input ~drop:(fun c -> Char.(=) c '\n')
+  |> String.split_lines
+  >>| String.to_list
+  |> List.transpose_exn
+  |> group_problems_2
+  >>| Problem.parse_2
 
 let solve input =
-  let problems = parse_input input in
-  printf "%i\n" (part1 problems)
+  let problems_1 = parse_input_1 input
+  and problems_2 = parse_input_2 input in
+  printf "%i %i\n" (solve problems_1) (solve problems_2)
--- lib/day06.mli
+++ lib/day06.mli
@@ -1,1 +1,16 @@
 val solve: string -> unit
+
+module Problem : sig
+  type operation = ADD | MUL [@@deriving compare, sexp]
+
+  type t = {
+    numbers : int list;
+    operation : operation;
+  } [@@deriving compare, sexp]
+
+  val parse_1 : string list -> t
+  val parse_2 : char list list -> t
+  val evaluate : t -> int
+end
+
+val parse_input_2 : string -> Problem.t list
