c736e5af37864b85e2440c858b6c4d578d75451b
Ksawery Radziwiłowicz <ksawery.radziwilowicz@gmail.com>
2025-12-07 10:25:51 +0100

    Day 07 Part 2

--- lib/day07.ml
+++ lib/day07.ml
@@ -15,7 +15,7 @@ module Simulation = struct
 
   type t = {
     diagram : diagram;
-    tachyons : (Position.t, Position_order.comparator_witness) Set.t;
+    tachyons : (Position.t, int, Position_order.comparator_witness) Map.t;
     splits : int;
   }
 
@@ -33,22 +33,26 @@ module Simulation = struct
       >>| Fun.compose (Array.map ~f:cell_of_char) String.to_array
       |> List.to_array in
     let start = Array.findi diagram.(0) ~f:(fun _ -> function | START -> true | _ -> false) |> Option.value_exn |> fst in
-    { diagram ; tachyons = Set.empty (module Position_order) |> (Fun.flip Set.add (start, 0)) ; splits = 0 }
-  
+    { diagram ; tachyons = Map.empty (module Position_order) |> (Map.set ~key:(start, 0) ~data:1) ; splits = 0 }
+
+  let add_tachyon m pos cnt = Map.update m pos ~f:(fun o -> cnt + Option.value o ~default:0)
+
   let rec simulate ?(depth = 0) ({ diagram ; tachyons ; splits } as state) =
     if depth = Array.length diagram - 1 then
       state
     else
       let width = Array.length diagram.(0) in
-      let (new_tachyons, new_splits) = Set.fold ~init:(Set.empty (module Position_order), splits) tachyons ~f:(fun (s, cnt) (x, y) -> match diagram.(y + 1).(x) with
-        | SPLITTER -> ((if x > 0 then Set.add s (x - 1, y + 1) else s) |> (fun s' -> (if x < width - 1 then Set.add s' (x + 1, y + 1) else s')), cnt + 1)
-        | _        -> (Set.add s (x, y + 1), cnt)
+      let (new_tachyons, new_splits) = Map.fold ~init:(Map.empty (module Position_order), splits) tachyons ~f:(fun ~key:(x, y) ~data:cnt (m, acc_splits) -> match diagram.(y + 1).(x) with
+        | SPLITTER -> ((if x > 0 then add_tachyon m (x - 1, y + 1) cnt else m) |> (fun m' -> (if x < width - 1 then add_tachyon m' (x + 1, y + 1) cnt else m')), acc_splits + 1)
+        | _        -> (add_tachyon m (x, y + 1) cnt, acc_splits)
       ) in
       simulate ~depth:(depth + 1) { diagram ; tachyons = new_tachyons ; splits = new_splits }
 end
 
-let part1 simulation = (Simulation.simulate simulation).splits
+let part1 { Simulation.splits ; _ } = splits
+
+let part2 { Simulation.tachyons ; _ } = Map.sum (module Int) tachyons ~f:Fun.id
 
 let solve input =
-  let simulation = Simulation.of_string input in
-    printf "%i\n" (part1 simulation)
+  let simulation = Simulation.of_string input |> Simulation.simulate in
+    printf "%i %i\n" (part1 simulation) (part2 simulation)
